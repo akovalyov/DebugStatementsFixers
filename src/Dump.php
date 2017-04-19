e0b078bdae1fa27c64a1a3df12b37a4b1816a4ef
Andrew Kovalyov <andrew.kovalyoff@gmail.com>
2017-04-19 17:15:11 +0300

    Fix undocumented BC for contrib fixers @see https://github.com/FriendsOfPHP/PHP-CS-Fixer/commit/0fe24381dabfc2cc881e643c95d0e10c1f46a43c

--- src/Dump.php
+++ src/Dump.php
@@ -20,33 +20,6 @@ final class Dump extends AbstractFunctionReferenceFixer
     /**
      * {@inheritdoc}
      */
-    public function fix(\SplFileInfo $file, Tokens $tokens)
-    {
-        foreach ($this->functions as $function) {
-            $currIndex = 0;
-            while (null !== $currIndex) {
-                $matches = $this->find($function, $tokens, $currIndex);
-
-                if (null === $matches) {
-                    break;
-                }
-
-                $funcStart = $tokens->getPrevNonWhitespace($matches[0]);
-
-                if ($tokens[$funcStart]->isGivenKind(T_NEW) || $tokens[$funcStart]->isGivenKind(T_FUNCTION)) {
-                    break;
-                }
-
-                $funcEnd = $tokens->getNextTokenOfKind($matches[1], array(';'));
-
-                $tokens->clearRange($funcStart + 1, $funcEnd);
-            }
-        }
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     public function isCandidate(Tokens $tokens)
     {
         return $tokens->isTokenKindFound(T_STRING);
@@ -69,5 +42,32 @@ final class Dump extends AbstractFunctionReferenceFixer
             'Removes dump/var_dump statements, which shouldn\'t be in production ever.',
             array(new CodeSample("<?php\nvar_dump(false);"))
         );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function applyFix(\SplFileInfo $file, Tokens $tokens)
+    {
+        foreach ($this->functions as $function) {
+            $currIndex = 0;
+            while (null !== $currIndex) {
+                $matches = $this->find($function, $tokens, $currIndex);
+
+                if (null === $matches) {
+                    break;
+                }
+
+                $funcStart = $tokens->getPrevNonWhitespace($matches[0]);
+
+                if ($tokens[$funcStart]->isGivenKind(T_NEW) || $tokens[$funcStart]->isGivenKind(T_FUNCTION)) {
+                    break;
+                }
+
+                $funcEnd = $tokens->getNextTokenOfKind($matches[1], array(';'));
+
+                $tokens->clearRange($funcStart + 1, $funcEnd);
+            }
+        }
     }
 }
